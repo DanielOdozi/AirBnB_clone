5273b528c04e04567ce641d7fa098e62ca8f43bb
root <root@DanielOdozi>
2023-12-10 20:27:29 +0100

    Create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -10,15 +10,24 @@ class BaseModel:
      Attributes:
         id (int): Identity of each instance.
     """
-    def __init__(self) -> None:
+    def __init__(self, *args, **kwargs) -> None:
         """Creates new instances of Base.
 
         Args:
             id (int, optional): Identity of each instance.
         """
-        self.updated_at = datetime.datetime.now()
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                if key in ['created_at', 'updated_at']:
+                    value = datetime.datetime.strptime(value,
+                                                       "%Y-%m-%dT%H:%M:%S.%f")
+                setattr(self, key, value)
+        else:
+            self.updated_at = datetime.datetime.now()
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
 
     def update(self):
         '''Update the time everytime object is called'''
